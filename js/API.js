c0d338bcf254041f3a2d61c0c684ffc59dc76050
Jorgeob18 <jorgealbertoob18@gmail.com>
2023-02-21 17:02:40 -0600

    Listar clientes de la API

--- js/API.js
+++ js/API.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 const url = 'http://localhost:4000/clientes';
-
+// Cuando se crea nuevo cliente
 export const nuevoCliente = async (cliente) => {
   try {
     fetch(url, {
@@ -13,5 +13,15 @@ export const nuevoCliente = async (cliente) => {
     window.location.href = 'index.html';
   } catch (error) {
     console.error(error);
+  }
+};
+// Obtiene todos los clientes
+export const obtenerClientes = async () => {
+  try {
+    const resultado = await fetch(url); // Si no se le especifica metodo toma por default GET
+    const clientes = await resultado.json();
+    return clientes;
+  } catch (error) {
+    console.log(error);
   }
 };
